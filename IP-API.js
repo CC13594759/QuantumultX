de97e06c0017f00e4a87f19de7d0cd2d24353c91
CC <59593391+CC13594759@users.noreply.github.com>
2026-06-20 10:02:03 +0800

    Modify subtitle to append IP address
    Updated subtitle to include IP address in the output.

--- IP-API.js
+++ IP-API.js
@@ -41,7 +41,7 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 var title = flags.get(obj['countryCode']) + ' ' + obj['country'] + ' ' + 'StarLink'
-var subtitle = obj['regionName'] + ' ' + obj['city'];
+var subtitle = obj['regionName'] + ' ' + obj['city'] + ' ' + obj['query'];
 
 var ip = obj['query'];
 var description = '\n';
